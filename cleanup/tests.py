161d85fef2c891ba784325e506aac8ac3366148c
MTmtEmpty <45623263+MTEmpty@users.noreply.github.com>
2022-03-21 22:32:55 +1000

    add tests for lols

--- cleanup/tests.py
+++ cleanup/tests.py
@@ -1,4 +1,8 @@
-from utils import seconds_to_days
+from utils import *
+
+
+def get_object_class_name(object_):
+    return object_.__class__.__name__
 
 
 def test_seconds_to_days():
@@ -8,5 +12,24 @@ def test_seconds_to_days():
     assert days == float(1)
 
 
-if __name__ == '__main__':
-    test_seconds_to_days()
+def test_get_config():
+    actual = get_config()
+    assert get_object_class_name(actual) == 'dict'
+    assert actual.get('config_path') is not None
+    
+    actual2 = get_cleanup_targets(actual)
+    assert get_object_class_name(actual2) == 'dict'
+    
+    actual_paths = actual2.get('paths')
+    assert actual_paths is not None
+    assert get_object_class_name(actual_paths) == 'dict'
+    assert len(actual_paths) > 0
+    
+    for key, _ in actual_paths.items():
+        path_config = actual_paths[key]
+        path = path_config.get('path')
+        delete_empty_folder = path_config.get('delete_empty_folder')
+        age_threshold = path_config.get('age_threshold')
+        assert path is not None
+        assert delete_empty_folder is not None
+        assert age_threshold is not None
